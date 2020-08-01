27ca24620ccbcc3b04ea5d8a517ec1bca55704c7
Felipe Ribeiro <emaildofeliperibeiro@gmail.com>
2020-08-01 19:12:56 -0300

    ajusting backend docker env

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -4,6 +4,25 @@ const rootPath = path.normalize(__dirname + "/..");
 const env = process.env.NODE_ENV || "production";
 
 const config = {
+    development: {
+        root: rootPath,
+        app: {
+            name: process.env.APP_NAME
+        },
+        port: process.env.PORT || 3000,
+        db: process.env.MONGODB_URI,
+        cognito: {
+            UserPoolId: process.env.COGNITO_USERPOOL_ID,
+            ClientId: process.env.COGNITO_CLIENT_ID
+        },
+        sqs: {
+            region: process.env.SQS_REGION || "us-east-1",
+            sqs_apiVersion: process.env.SQS_API_VERSION,
+            queueURL: process.env.QUEUE_URL,
+            DelaySeconds: process.env.SQS_DELAY,
+        },
+        activeSendMessageToSQS: process.env.SQS_ACTIVE
+    },
     production: {
         root: rootPath,
         app: {
